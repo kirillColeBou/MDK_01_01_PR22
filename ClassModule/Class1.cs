151f20d9791deeab72fae43435215f1423cded72
Kirill <kirillteplycov888@gmail.com>
2023-12-03 20:32:27 +0500

    Разработанные классы. Тепляков

---
+++ ClassModule/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassModule
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string phone_num { get; set; }
+        public string fio_user { get; set; }
+        public string passport_data { get; set; }
+    }
+
+    public class Call
+    {
+        public int id { get; set; }
+        public int user_id { get; set; }
+        public int category_call { get; set; }
+        public string data { get; set; }
+        public string time_start { get; set; }
+        public string time_end { get; set;}
+    }
+}
